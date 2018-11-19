37a3d9a6107e059dd253ee7221a6121d4f0aa3f4
Maxi <maximilian-bergbauer@web.de>
2018-11-19 17:02:58 +0100

    login Page added

--- app/src/main/java/org/wit/archaeologicalfieldwork/MainActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/MainActivity.kt
@@ -2,6 +2,8 @@ package org.wit.archaeologicalfieldwork
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         info("MainActivity started...")
-    }
+
+        login_btn.setOnClickListener{
+            var status = if(usereamil_et.text.toString().equals("maximilian-bergbauer@web.de")
+            && password_et.text.toString().equals("1234")){
+                "Logged in successfully"
+            }else{
+                "LogIn Failed"
+            }
+            Toast.makeText(this,status,Toast.LENGTH_SHORT).show()
+        }
+    }@
 }
