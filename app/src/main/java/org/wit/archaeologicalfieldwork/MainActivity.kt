65202b5326126dc1e6d2d68ba2e68f3958fedb0b
Maxi <maximilian-bergbauer@web.de>
2018-11-19 16:11:11 +0100

    SplashScreen added

--- app/src/main/java/org/wit/archaeologicalfieldwork/MainActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/MainActivity.kt
@@ -2,11 +2,15 @@ package org.wit.archaeologicalfieldwork
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        info("MainActivity started...")
     }
 }
---
+++ app/src/main/java/org/wit/archaeologicalfieldwork/SplashScreenActivity.kt
@@ -0,0 +1,29 @@
+package org.wit.archaeologicalfieldwork
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import java.lang.Exception
+
+class SplashScreenActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val SPLASH_TIME_OUT = 4000
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        Handler().postDelayed(object:Runnable{
+             override fun run(){
+                val home = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                 startActivity(home)
+                 finish()
+            }
+        },SPLASH_TIME_OUT.toLong())
+
+
+
+    }
+}
