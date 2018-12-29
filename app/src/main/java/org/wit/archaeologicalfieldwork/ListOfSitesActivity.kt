ff0d3a9f5a8ddc2ab0be4a990fe0af91ceee906d
unknown <maximilian-bergbauer@web.de>
2018-12-29 15:11:17 +0100

    noMenueIconsShown in Toolbar

--- app/src/main/java/org/wit/archaeologicalfieldwork/ListOfSitesActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/ListOfSitesActivity.kt
@@ -2,7 +2,6 @@ package org.wit.archaeologicalfieldwork
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
 import android.view.MenuItem
 
 
---
+++ app/src/main/java/org/wit/archaeologicalfieldwork/MyChildActivity.kt
@@ -0,0 +1,4 @@
+package org.wit.archaeologicalfieldwork
+
+class MyChildActivity {
+}
