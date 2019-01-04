0713d3274b0d2ca35fba6f334ef358bfb348f28b
unknown <maximilian-bergbauer@web.de>
2019-01-04 16:42:34 +0100

    LoginActivity is working

--- app/src/main/java/org/wit/archaeologicalfieldwork/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/archaeologicalfieldwork/activities/LoginActivity.kt
@@ -1,9 +1,12 @@
 package org.wit.archaeologicalfieldwork.activities
 
+import android.content.Intent
 import android.os.Bundle
 
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import org.wit.archaeologicalfieldwork.R
 
@@ -19,6 +22,22 @@ class LoginActivity : AppCompatActivity(){
             val password = password_edittext_login.text.toString()
 
             Log.d("Login","Attempt login with email/pw $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful){
+
+                        return@addOnCompleteListener
+                    }else {
+                        Toast.makeText(this,"Successfully signed in", Toast.LENGTH_SHORT).show()
+                        Log.d("Main", "Successfully signed in: ${it.result.user.uid}")
+                        val intent = Intent(this,SiteListActivity::class.java)
+                        startActivity(intent)
+                }
+        }.addOnFailureListener {
+                    Log.d("Main","Faild to sign in${it.message}")
+                    Toast.makeText(this,"Faild to sign in", Toast.LENGTH_SHORT).show()
+                }
 
         }
 
